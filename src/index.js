0521de254442145299914e0d97a00c869d9a8215
TMV <m.timarsuev@gmail.com>
2022-12-04 13:10:39 +0300

    first test react component

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const BookList = () => <section>BookList component!</section>;
+
+ReactDOM.render(<BookList />, document.getElementById(`root`));
