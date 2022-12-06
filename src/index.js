60882b3cf78f70d205ce8b90dab12afd5a7b7775
TMV <m.timarsuev@gmail.com>
2022-12-06 14:23:05 +0300

    Updated component

--- src/index.js
+++ src/index.js
@@ -5,56 +5,96 @@ import ReactDOM from "react-dom";
 // Import CSS
 import "./index.css";
 
+const booksData = [
+  {
+    id: 1,
+    imgUrl: `https://d2mk45aasx86xg.cloudfront.net/Best_React_Books_Learning_React_by_Alex_Banks_and_Eve_Porcello_2_11zon_98b601256f.webp`,
+    title: `Learning React`,
+    author: `Alex Banks and Eve Porcello`,
+  },
+  {
+    id: 2,
+    imgUrl: `https://m.media-amazon.com/images/P/B0B6XFT4RH.01._SCLZZZZZZZ_SX500_.jpg`,
+    title: `Interesting Facts For Curious Minds`,
+    author: `Jordan Moore`,
+  },
+  {
+    id: 3,
+    imgUrl: `https://m.media-amazon.com/images/P/1951806409.01._SCLZZZZZZZ_SX500_.jpg`,
+    title: `Life Skills for Teens: How to Cook, Clean, Manage Money`,
+    author: `Karen Harris`,
+  },
+  {
+    id: 4,
+    imgUrl: `https://m.media-amazon.com/images/P/1733702253.01._SCLZZZZZZZ_SX500_.jpg`,
+    title: `The Screaming Goat`,
+    author: `Screaming Goat Press`,
+  },
+  {
+    id: 5,
+    imgUrl: `https://m.media-amazon.com/images/P/1733702202.01._SCLZZZZZZZ_SX500_.jpg`,
+    title: `Cat Butts In Space (The Feline Frontier!)`,
+    author: `Val Brains`,
+  },
+  {
+    id: 6,
+    imgUrl: `https://m.media-amazon.com/images/I/51xI1CeUOlL._AC_SY780_.jpg`,
+    title: `A World of Curiosities: A Novel`,
+    author: `Louise Penny`,
+  },
+  {
+    id: 7,
+    imgUrl: `https://m.media-amazon.com/images/I/51wux7NzjJL._AC_SY780_.jpg`,
+    title: `Roses, in the Mouth of a Lion`,
+    author: `Bushra Rehman`,
+  },
+];
+
 // Book list component
 const BookList = () => (
   <React.Fragment>
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      {booksData.map((book) => (
+        <Book key={book.id} {...book} />
+      ))}
     </section>
   </React.Fragment>
 );
 
-const Book = () => (
+const Book = ({ imgUrl, title, author }) => {
+  return (
+    <React.Fragment>
+      <article className="book">
+        <BookImg url={imgUrl} altName={`${title}CoverImg`} />
+        <BookTitle title={title} />
+        <BookAuthor author={author} />
+      </article>
+    </React.Fragment>
+  );
+};
+
+const BookImg = ({ url, altName }) => (
   <React.Fragment>
-    <article className="book">
-      <BookImg />
-      <BookTitle />
-      <BookAuthor />
-    </article>
+    <img src={url} alt={altName} />
   </React.Fragment>
 );
 
-const BookImg = () => (
+const BookTitle = ({ title }) => (
   <React.Fragment>
-    <img
-      src="https://d2mk45aasx86xg.cloudfront.net/Best_React_Books_Learning_React_by_Alex_Banks_and_Eve_Porcello_2_11zon_98b601256f.webp"
-      alt="ReactBookCover"
-      width={300}
-      height={400}
-    />
+    <h3>{title}</h3>
   </React.Fragment>
 );
 
-const BookTitle = () => (
+const BookAuthor = ({ author }) => (
   <React.Fragment>
-    <h3>Learning React</h3>
-  </React.Fragment>
-);
-
-const BookAuthor = () => (
-  <React.Fragment>
-    <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
-      Alex Banks and Eve Porcello
-    </h4>
+    <h4>{author}</h4>
   </React.Fragment>
 );
 
 ReactDOM.render(<BookList />, document.getElementById(`root`));
+
+// const author = `Alex Banks and Eve Porcello`;
+// const title = `Learning React`;
+// const img =
+//   "https://d2mk45aasx86xg.cloudfront.net/Best_React_Books_Learning_React_by_Alex_Banks_and_Eve_Porcello_2_11zon_98b601256f.webp";
+// const imgName = "ReactBookCover";
